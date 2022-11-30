3055219ef473ec894da71a324f90dccc9b4e638f
andrewcargill <andycargill01@aol.com>
2022-11-30 12:36:59 +0000

    Create a basic function

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello From React</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>
+export default FunctionalGreeting
+
+// default means that we can import is as another name
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello From React</h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1>Hello with props from React!</h1>
+export default FunctionalGreetingWithProps
+
+// default means that we can import is as another name
