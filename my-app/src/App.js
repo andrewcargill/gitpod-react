51819b8539f6e2ca669bf9f77b757715bf3fb0c5
andrewcargill <andycargill01@aol.com>
2022-12-01 07:10:00 +0000

    state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import StatefulGreeting from './components/StatefulGreeting';
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I am this" name="dave" />
+      <StatefulGreeting greeting="I am happy" name="dave" />
     </div>
   );
 }
--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state ={
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
     render() {
-    return <h1>Hello from me {this.props.greeting}. Your name is {this.props.name}</h1>
+    return (
+        <div>
+            <h1>{this.state.introduction} {this.props.greeting}. Your name is {this.props.name}.</h1>
+            <button>{this.state.buttonText }</button>
+        </div>
+    )
 }
 }
 
