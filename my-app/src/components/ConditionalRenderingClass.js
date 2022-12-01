d053c37c64ea5ded0c2c2c406473d7e66421aab4
andrewcargill <andycargill01@aol.com>
2022-12-01 11:43:37 +0000

    Rendering depending on state and props

--- my-app/src/components/ConditionalRenderingClass.js
+++ my-app/src/components/ConditionalRenderingClass.js
@@ -5,7 +5,8 @@ export class ConditionalRenderingClass extends Component {
       super(props)
     
       this.state = {
-         isLoaded: true
+         isLoaded: true,
+         isLoggedIn: false
       }
     }
   render() {
@@ -17,6 +18,16 @@ export class ConditionalRenderingClass extends Component {
         </div>
         <div>
             <h1>{this.state.isLoaded ? 'Data Loaded' : 'Loading... '}</h1>
+            <h2>{this.state.isLoggedIn ? (
+                <div>
+                    <p>welcome to the site. Please complete the steps below:</p>
+                    <ol>
+                        <li>Confirm Email</li>
+                        <li>Complete Profile</li>
+                        <li>Subscribe to newsletter</li>
+                    </ol>
+                </div>
+            ): (<p>Please Sign In</p> )}</h2>
         </div>
 
         </div>
